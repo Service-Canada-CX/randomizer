e0eb7420582190a2c5c42b98ede9e753059b8786
Guillaume Clement <clement.guillaume@live.ca>
2019-05-02 16:54:45 -0400

    Adding link templating and i18n support. Implemented a few simple JS functions to support "templating" and query string parameters.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 We use this randomizer when we're running multiple studies at once.
 
+Usage: https://_username_.github.io/randomizer/_page_.html?lang=en
--- js/randomizer.js
+++ js/randomizer.js
@@ -1,14 +1,42 @@
 /**
-* Link redirection randomizer function
-*
+* Define query string and
+* template params
 */
-var randomLinkRedirect = function (links) {
+var LANG_PARAM = "lang=";
+var LANG_TEMPLATE = "${lang}";
+
+
+/**
+* Link redirection randomizer function with
+* support for i18n.
+*/
+function randomLinkRedirectI18N(links, lang) {
     // Count the links
     var max = (links.length);
     // Generate a random number
     var randomNumber = Math.floor(Math.random()*max);
     // Use the random number to pick a URL
     var link = links[randomNumber];
+    //Insert i18n
+    link = link.replace(LANG_TEMPLATE, lang)
     // change the location of the window object
     window.location = link;
-}
+};
+
+/**
+* Link redirection randomizer vanity
+* function with no i18n support
+*/
+function randomLinkRedirect(links) {
+  randomLinkRedirectI18N(links, "");
+};
+
+/**
+* Get query string param, with only basic JS support
+* for non evergreen browsers.
+* @see https://davidwalsh.name/query-string-javascript
+*/
+function getLangUrlParameter() {
+    var lang = window.location.href;
+    return lang.slice(lang.indexOf(LANG_PARAM)+LANG_PARAM.length);
+};
